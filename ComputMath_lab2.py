c333cd2d301f269c990dddca0d299ef433001c34
Alex <46602606+IKdotShark@users.noreply.github.com>
2024-02-09 19:49:51 +0500

    lab2

---
+++ ComputMath_lab2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as mathplot
+
+def parametric_function(t):
+    x = 2 * np.sin(2* t)
+    y = 2* np.cos(t)
+    return x, y
+
+def graph(x,y):
+    mathplot.plot(x, y)
+    mathplot.title('Параметрический график функции')
+    mathplot.xlabel('x')
+    mathplot.ylabel('y')
+    mathplot.grid(True)
+    mathplot.show()
+
+# Задаем диапазон параметра t
+t = np.linspace(0, 2 * np.pi, 100)
+# Вызываем функцию для получения значений x и y
+x, y = parametric_function(t)
+
